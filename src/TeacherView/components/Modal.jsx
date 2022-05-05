31d83e851d07302cf1a7b10fc57890637e7041cc
tshepi-tech <tshepi.tech@gmail.com>
2022-05-05 06:19:26 +0200

    make formappear on modal

--- src/TeacherView/components/Modal.jsx
+++ src/TeacherView/components/Modal.jsx
@@ -12,7 +12,7 @@ export default function Modal() {
   return createPortal(
     <div id="modal">
       <div className="background">{/* empty on purpose */}</div>
-      <div className="content">Modal</div>
+      <div className="content">{modal}</div>
     </div>,
     document.getElementById("portal")
   );
