7f5b0ef32b8d0510095b6987bab33a8f73226d8a
tshepi-tech <tshepi.tech@gmail.com>
2022-05-08 21:05:56 +0200

    landing page buttons

--- src/AuthPages/Landing.jsx
+++ src/AuthPages/Landing.jsx
@@ -5,11 +5,17 @@ export default function Landing() {
   return (
     <div>
       <h1>This is an awesome LMS system</h1>
-      <Link to="/signup">Create account</Link>
+      <Link to="/signup">
+        <button>Create account</button>
+      </Link>
       <br></br>
-      <Link to="/login">Log In</Link>
+      <Link to="/login">
+        <button>Log In</button>
+      </Link>
       <br></br>
-      <Link to="/resetpassword">Reset password</Link>
+      <Link to="/resetpassword">
+        <button>Reset password</button>
+      </Link>
     </div>
   );
 }
